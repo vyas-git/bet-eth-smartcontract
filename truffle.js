a16b17c8527f932ea637c04715a1226d3622d828
vyas-git <vyasreddy.tech@gmail.com>
2022-12-04 09:33:25 +0530

    config js files

--- truffle.js
+++ truffle.js
@@ -1,8 +1,5 @@
-var WalletProvider = require("truffle-wallet-provider");
-const Wallet = require('ethereumjs-wallet');
-var goerliPrivateKey = new Buffer("dee237f41369adae7cbf4cbff3b26a0759b307eafcaa431ce79e9913e80bb2dd", "hex");
-var goerliWallet = Wallet.fromPrivateKey(goerliPrivateKey);
-var goerliProvider = new WalletProvider(goerliWallet, "https://goerli.infura.io/v3/eb89e20105b44988becedc298bc4c70e");
+var WalletProvider = require("@truffle/hdwallet-provider");
+var goerliProvider = new WalletProvider({ privateKeys: ["YOUR_PRIVATE_KEY"], providerOrUrl: `wss://goerli.infura.io/ws/v3/${YOUR - INFURA - API - KEY}` });
 
 module.exports = {
 	// See <http://truffleframework.com/docs/advanced/configuration>
